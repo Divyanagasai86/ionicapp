104a04fe9ea0cf4f5ceeeda26e69da4dc7443c45
Divya Naga Sai Tanuku <divya.tanuku@rampgroup.com>
2022-07-13 12:52:47 +0530

    added splashscreen and spinners

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,24 @@
-import { IonButton, IonContent, IonGrid, IonRow,IonImg, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-
+import { IonButton, IonContent, IonGrid, IonRow, IonImg, IonPage, IonTitle, IonToolbar,IonLoading } from '@ionic/react';
+import { useState } from "react";
 import './Home.css';
 
 
 
 const Home = () => {
+
   return (
     <IonPage >
-      <IonContent fullscreen  className="content-page" >
+      <IonContent fullscreen className="content-page" >
         <IonGrid className="content-grid">
           <IonRow className="hometitle">
-          <IonTitle > BEST OFFLINE DICTIONARY...</IonTitle>
+            <IonTitle > BEST OFFLINE DICTIONARY...</IonTitle>
           </IonRow>
           <IonRow className="homesubtitle">
-          <IonTitle>For iOS, ANDRIOD,Windows</IonTitle>
+            <IonTitle>For iOS, ANDRIOD,Windows</IonTitle>
           </IonRow>
           <IonRow className="grid-row3">
-          <IonButton className="get-started" shape="round" routerLink="/Login">Get Started</IonButton>
+
+            <IonButton className="get-started" shape="round" routerLink="/Login">Get Started</IonButton>
           </IonRow>
         </IonGrid>
       </IonContent>
